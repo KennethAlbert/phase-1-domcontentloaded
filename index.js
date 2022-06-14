cee13e6f6352820f5e931f33c3930735feaac01c
Kenneth Albert <gichuka@gmail.com>
2022-06-14 10:06:00 +0300

    commit tree 9fa5456a0df0f7ac700eedbe72d6b37f59667862

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",()=>{
+  let p=document.querySelector("#text");
+  let p1=p.innerHTML.replace("JavaScript is so cool. It lets me add text to my page programmatically.", "This is really cool!")
+   p.innerHTML=p1;   
+});
+
